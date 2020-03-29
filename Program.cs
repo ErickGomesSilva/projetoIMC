27f7b05277070fc738fb2acdd33cdb672dd69bdc
unknown <erick.jgomes.dasilva@gmail.com>
2020-03-29 18:02:59 -0300

    Criado Projeto IMC

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMC_Calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double peso;
+            double altura;
+           
+            
+            Console.WriteLine("Informe seu peso");
+            peso = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Informe sua Altura");
+            altura = Convert.ToDouble(Console.ReadLine());      
+
+
+            Console.WriteLine("IMC: " + (peso / (altura * altura)));
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
